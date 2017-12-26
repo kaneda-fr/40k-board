cc8222035bb61c14e7c1cfc739f2dd645308f657
Sebastien Lacoste-Seris <sebastien.lacoste-seris@sita.aero>
2017-12-26 20:59:28 +0100

    re-added panel display using joueur component

--- src/app/joueur/joueur.component.ts
+++ src/app/joueur/joueur.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { joueur } from '../models';
+import { ApiService } from '../services';
 
 @Component({
   selector: 'app-joueur',
@@ -7,12 +8,19 @@ import { joueur } from '../models';
   styleUrls: ['./joueur.component.css']
 })
 export class JoueurComponent implements OnInit {
+  @Input() nom: string;
+  joueur: joueur[];
 
-  joueur: joueur;
-
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.getJoueur(this.nom);
+  }
+
+   getJoueur(nom?: string): void {
+    console.log('Loading Joueur ' + nom);
+     this.apiService.joueurNomGET(nom)
+     .subscribe(joueur => this.joueur = joueur);
   }
 
 }
